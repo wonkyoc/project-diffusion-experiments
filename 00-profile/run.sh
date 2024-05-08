95052838ba1dd6640d8d289f435a407e25e64727
Wonkyo Choe <wonkyochoe@virginia.edu>
2024-05-08 12:12:49 -0400

    edit 00 profile

--- 00-profile/run.sh
+++ 00-profile/run.sh
@@ -2,14 +2,16 @@
 
 
 #EXP_DIR=$(dirname $(readlink -f $0))
-HOME=/home/wonkyoc/git
+#HOME=/home/wonkyoc/git
+HOME=/p/alpha
 MODEL_DIR=$HOME/models
-EXP_DIR=$HOME/diffusion-experiments
+EXP_DIR=$HOME/project-diffusion-experiments
 SDCPP_DIR=$HOME/stable-diffusion.cpp
 DIFF_DIR=$HOME/diffusers
+LOG_DIR=$HOME/project-diffusion-experiments/00-profile/results
 
 # hyperparameters
-THREADS=24
+THREADS=(1 2 4 8 12 20 24)
 STEPS=1
 PROMPT="an oil surrealist painting of a dreamworld on a seashore where clocks and watches appear to be inexplicably limp and melting in the desolate landscape. a table on the left, with a golden watch swarmed by ants. a strange fleshy creature in the center of the painting"
 
@@ -17,21 +19,22 @@ PROMPT="an oil surrealist painting of a dreamworld on a seashore where clocks an
 run_diffusers() {
     echo "Run Diffusers"
     python $EXP_DIR/01-parallelism/main.py \
-        --prompt "$PROMPT" \
-        --threads $THREADS \
-        --steps $STEPS
+        --prompt "$1" \
+        --threads $2 \
+        --steps $3 > $LOG_DIR/diffusers-t$2.log
 }
 
 run_sdcpp() {
     echo "Run stable-diffusion.cpp"
     $SDCPP_DIR/build/bin/sd -m $MODEL_DIR/sd-v1-5.ckpt \
         --sampling-method euler_a \
-        --prompt "$PROMPT" \
-        --threads $THREADS    \
-        --steps $STEPS   \
-        -v --color
+        --prompt "$1" \
+        --threads $2    \
+        --steps $3   \
+        -v --color > $LOG_DIR/sdcpp-t$2.log
 }
 
-run_diffusers
-run_sdcpp
-
+for t in ${THREADS[@]}; do
+    run_diffusers $PROMPT $t $STEPS
+    run_sdcpp $PROMPT $t $STEPS
+done
