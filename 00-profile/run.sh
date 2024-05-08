5fcfb8d18d57a96a5bc7f4604ca735e6e4b7007f
Wonkyo Choe <wonkyochoe@virginia.edu>
2024-05-08 12:27:27 -0400

    edit 00 profile

--- 00-profile/run.sh
+++ 00-profile/run.sh
@@ -11,7 +11,7 @@ DIFF_DIR=$HOME/diffusers
 LOG_DIR=$HOME/project-diffusion-experiments/00-profile/results
 
 # hyperparameters
-THREADS=(1 2 4 8 12 20 24)
+THREADS=(1 2 4 8 12 20)
 #PIN="taskset -c 4-7"
 STEPS=1
 PROMPT="an oil surrealist painting of a dreamworld on a seashore where clocks and watches appear to be inexplicably limp and melting in the desolate landscape. a table on the left, with a golden watch swarmed by ants. a strange fleshy creature in the center of the painting"
@@ -20,22 +20,22 @@ PROMPT="an oil surrealist painting of a dreamworld on a seashore where clocks an
 run_diffusers() {
     echo "Run Diffusers"
     $PIN python $EXP_DIR/01-parallelism/main.py \
-        --prompt "$1" \
-        --threads $2 \
-        --steps $3 > $LOG_DIR/diffusers-t$2.log
+        --prompt "$PROMPT" \
+        --threads $1 \
+        --steps $2 > $LOG_DIR/$HOSTNAME-diffusers-t$2.log
 }
 
 run_sdcpp() {
     echo "Run stable-diffusion.cpp"
     $PIN $SDCPP_DIR/build/bin/sd -m $MODEL_DIR/sd-v1-5.ckpt \
         --sampling-method euler_a \
-        --prompt "$1" \
-        --threads $2    \
-        --steps $3   \
-        -v --color > $LOG_DIR/sdcpp-t$2.log
+        --prompt "$PROMPT" \
+        --threads $1    \
+        --steps $2   \
+        -v --color > $LOG_DIR/$HOSTNAME-sdcpp-t$2.log
 }
 
 for t in ${THREADS[@]}; do
-    run_diffusers $PROMPT $t $STEPS
-    run_sdcpp $PROMPT $t $STEPS
+    run_diffusers $t $STEPS
+    run_sdcpp $t $STEPS
 done
